629e6cfe6aa392661895ec3ecc0e8810981e306b
Jana <janaahmedsaad@gmail.com>
2025-04-30 19:24:04 +0300

    Double sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import {SettingsComponent} from './settings/settings.component';
+import {GeneralComponent} from './settings/general/general.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {SellingOptionsComponent} from './settings/selling-options/selling-options.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+    children: [
+      {
+        path: 'general',
+        component: GeneralComponent,
+      },
+      {
+        path: 'selling-options',
+        component: SellingOptionsComponent,
+      },
+      { path: '', redirectTo: 'general', pathMatch: 'full' }
+    ]
+  }
+];
