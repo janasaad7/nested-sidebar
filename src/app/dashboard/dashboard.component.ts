8d83b862bdbdd8af1f188e59adf34282a4cf13c4
Jana <janaahmedsaad@gmail.com>
2025-04-30 22:53:15 +0300

    Changes in dimensions

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+    imports: [
+        NavbarComponent,
+    ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
